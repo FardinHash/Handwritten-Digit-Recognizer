0b283910b3048197590d270bf15df536746923ca
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-08-23 01:23:48 +0600

    Update raw-code.py

--- raw-code.py
+++ raw-code.py
@@ -12,3 +12,10 @@ from sklearn.metrics import accuracy_score
 
 #load the data
 (X_train, y_train), (X_test, y_test)= mnist.load_data()
+
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape((X_train.shape[0], num_pixels)).astype('float32')
+X_test = X_test.reshape((X_test.shape[0], num_pixels)).astype('float32')
+
+X_train = X_train / 255
+X_test = X_test / 255
