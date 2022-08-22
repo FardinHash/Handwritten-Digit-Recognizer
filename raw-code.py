14aa779f03f027c9369e7e9f699840007823a9f8
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-08-23 01:23:27 +0600

    Update raw-code.py

--- raw-code.py
+++ raw-code.py
@@ -9,3 +9,6 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
 
 from sklearn.metrics import accuracy_score
+
+#load the data
+(X_train, y_train), (X_test, y_test)= mnist.load_data()
